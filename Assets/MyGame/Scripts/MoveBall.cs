15606b7815822148bfe4ab588743d823319ce196
Twanghofer <tobias.w01@gmx.at>
2020-01-09 14:47:02 +0100

    created the ui layout, the ball and a script that chooses one of 4 given positions randomly

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    public Transform[] positions;
+    public Transform ball;
+
+    public void RandomPosition()
+    {
+        ball.position = positions[Random.Range(0, positions.Length)].position;
+    }
+
+}
